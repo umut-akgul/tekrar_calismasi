7bc7a411e3d1eb4470ed4948ade2326ac0216a89
“umut-akgul” <umut.akgul@gmail.com>
2025-06-30 10:12:01 +0300

    tasarim_sayfasi_sablonu_olusturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    ); 
-    
+      debugShowCheckedModeBanner: false,
+      home: TasarimTekrar(),
+    );
+  }
+}
+
+class TasarimTekrar extends StatefulWidget {
+  const TasarimTekrar({super.key});
+
+  @override
+  State<TasarimTekrar> createState() =>
+      _TasarimTekrarState();
+}
+
+class _TasarimTekrarState
+    extends State<TasarimTekrar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tasarım Tekrar"),
+        backgroundColor: Colors.pink,
+        foregroundColor: Colors.white,
+      ),
+    );
   }
 }
