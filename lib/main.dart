a61c70599a21dc4cd05cd85265fe5c3fe6dd3242
“umut-akgul” <umut.akgul@gmail.com>
2025-07-02 14:48:21 +0300

    listview_seperated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tekrar_calismasi/models/ogrenci.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +27,15 @@ class TasarimTekrar extends StatefulWidget {
 
 class _TasarimTekrarState
     extends State<TasarimTekrar> {
+  List<Ogrenci> tumOgrenciler = List.generate(
+    5000,
+    (index) => Ogrenci(
+      id: index + 1,
+      isim: "Öğrenci adı: ${index + 1}",
+      soyisim: "Soyisim: ${index + 1}",
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +43,61 @@ class _TasarimTekrarState
         title: Text("Tasarım Tekrar"),
         backgroundColor: Colors.pink,
         foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            Ogrenci ogr = tumOgrenciler[index];
+            return veriAlanSabitEleman(ogr);
+          },
+          separatorBuilder: (context, index) {
+            return (index + 1) % 4 == 0
+                ? Divider(
+                    color: Colors.green,
+                    thickness: 3,
+                  )
+                : Container();
+          },
+          itemCount: tumOgrenciler.length,
+        ),
+      ),
+    );
+  }
+
+  SizedBox veriAlanSabitEleman(Ogrenci ogr) {
+    return SizedBox(
+      height: 90,
+      child: Row(
+        children: [
+          Expanded(
+            child: Card(
+              color: Colors.limeAccent,
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(10),
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(
+                    child: Text(
+                      ogr.id.toString(),
+                    ),
+                  ),
+                  title: Text(ogr.isim),
+                  subtitle: Text(ogr.soyisim),
+                  trailing: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/models/ogrenci.dart
@@ -0,0 +1,11 @@
+class Ogrenci {
+  final int id;
+  final String isim;
+  final String soyisim;
+
+  Ogrenci({
+    required this.id,
+    required this.isim,
+    required this.soyisim,
+  });
+}
