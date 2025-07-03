7db364c9766e8eeb4ccf2bdebbe6239c37963a9e
“umut-akgul” <umut.akgul@gmail.com>
2025-07-03 13:25:48 +0300

    customscrollview_ve_sliverappbar

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,7 @@ class TasarimTekrar extends StatefulWidget {
       _TasarimTekrarState();
 }
 
-class _TasarimTekrarState
-    extends State<TasarimTekrar> {
+class _TasarimTekrarState extends State<TasarimTekrar> {
   List<Ogrenci> tumOgrenciler = List.generate(
     5000,
     (index) => Ogrenci(
@@ -39,28 +38,116 @@ class _TasarimTekrarState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Tasarım Tekrar"),
-        backgroundColor: Colors.pink,
-        foregroundColor: Colors.white,
+      backgroundColor: Colors.black54,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            title: Text("SliverAppBar"),
+            backgroundColor: Colors.black,
+            expandedHeight: 300,
+            foregroundColor: Colors.white,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                "Sliverappbar",
+                style: TextStyle(color: Colors.white),
+              ),
+              background: Image.network(
+                "https://cdnb.artstation.com/p/assets/images/images/036/276/145/large/anirban-sarkar-0008.jpg?1617206829",
+              ),
+            ),
+          ),
+          sliverlistDynamicOrnek(),
+          SliverPadding(
+            padding: EdgeInsetsGeometry.symmetric(
+              vertical: 30,
+            ),
+            sliver: SliverGrid(
+              delegate: SliverChildListDelegate([
+                sabitGridViewEleman(Colors.teal.shade400),
+                sabitGridViewEleman(Colors.teal.shade500),
+                sabitGridViewEleman(Colors.teal.shade500),
+                sabitGridViewEleman(Colors.teal.shade400),
+                sabitGridViewEleman(Colors.teal.shade400),
+                sabitGridViewEleman(Colors.teal.shade500),
+                sabitGridViewEleman(Colors.teal.shade500),
+                sabitGridViewEleman(Colors.teal.shade400),
+              ]),
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+            ),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: ListView.separated(
-          itemBuilder: (context, index) {
-            Ogrenci ogr = tumOgrenciler[index];
-            return veriAlanSabitEleman(ogr);
+    );
+  }
+
+  SliverPadding sliverlistDynamicOrnek() {
+    return SliverPadding(
+      padding: EdgeInsetsGeometry.all(10),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          childCount: 20,
+          (context, index) {
+            return dinamikGridElemani(index);
           },
-          separatorBuilder: (context, index) {
-            return (index + 1) % 4 == 0
-                ? Divider(
-                    color: Colors.green,
-                    thickness: 3,
-                  )
-                : Container();
-          },
-          itemCount: tumOgrenciler.length,
         ),
+      ),
+    );
+  }
+
+  GridView gridviewDynamicExtentOrnek() {
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: 8,
+      gridDelegate:
+          SliverGridDelegateWithMaxCrossAxisExtent(
+            crossAxisSpacing: 25,
+            mainAxisSpacing: 20,
+            maxCrossAxisExtent: 220,
+          ),
+      itemBuilder: (context, index) {
+        return dinamikGridElemani(index);
+      },
+    );
+  }
+
+  GridView gridviewExtentOrnek() {
+    return GridView.extent(
+      maxCrossAxisExtent: 80,
+      padding: EdgeInsets.all(10),
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 40,
+      children: [
+        sabitGridViewEleman(Colors.red),
+        sabitGridViewEleman(Colors.green),
+        sabitGridViewEleman(Colors.brown),
+        sabitGridViewEleman(Colors.blue),
+        sabitGridViewEleman(Colors.black),
+        sabitGridViewEleman(Colors.grey),
+        sabitGridViewEleman(Colors.purple),
+      ],
+    );
+  }
+
+  Padding seperatedListviewOrnek() {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: ListView.separated(
+        itemBuilder: (context, index) {
+          Ogrenci ogr = tumOgrenciler[index];
+          return veriAlanSabitEleman(ogr);
+        },
+        separatorBuilder: (context, index) {
+          return (index + 1) % 4 == 0
+              ? Divider(color: Colors.green, thickness: 3)
+              : Container();
+        },
+        itemCount: tumOgrenciler.length,
       ),
     );
   }
@@ -75,17 +162,14 @@ class _TasarimTekrarState
               color: Colors.limeAccent,
               elevation: 10,
               shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(10),
               ),
               child: Align(
                 alignment: Alignment.center,
                 child: ListTile(
                   onTap: () {},
                   leading: CircleAvatar(
-                    child: Text(
-                      ogr.id.toString(),
-                    ),
+                    child: Text(ogr.id.toString()),
                   ),
                   title: Text(ogr.isim),
                   subtitle: Text(ogr.soyisim),
@@ -99,6 +183,22 @@ class _TasarimTekrarState
           ),
         ],
       ),
+    );
+  }
+
+  Widget sabitGridViewEleman(Color bgColor) {
+    return Container(
+      height: 150,
+      color: bgColor,
+      child: Text("Deneme"),
+    );
+  }
+
+  dinamikGridElemani(int index) {
+    return Container(
+      height: 100,
+      color: Colors.teal[((((index + 1)) % 8) + 1) * 100],
+      child: Text("Deneme"),
     );
   }
 }
